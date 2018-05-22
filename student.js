b2be03c0502e0cbbe173cf68208b555c3b79dab2
Ken Hall <kenhallthe3rd@gmail.com>
2018-05-22 13:49:12 -0700

    Student add/delete features working

--- student.js
+++ student.js
@@ -3,7 +3,7 @@ module.exports = function(){
     var router = express.Router();
 
     function getStudents(res, mysql, context, complete){
-        mysql.pool.query("SELECT student.name, student.blood, house.tower, location.place FROM student INNER JOIN house ON house.house_id = student.dorm INNER JOIN location ON location.location_id = student.locatedAt", function(error, results, fields){
+        mysql.pool.query("SELECT student.student_id, student.name, student.blood, house.tower, location.place FROM student INNER JOIN house ON house.house_id = student.dorm INNER JOIN location ON location.location_id = student.locatedAt", function(error, results, fields){
             if(error){
                 res.end();
             }
@@ -12,6 +12,38 @@ module.exports = function(){
             
         });
     }
+
+    router.post('/delete', function(req, res){
+        var mysql = req.app.get('mysql');
+        var sql = "DELETE FROM student WHERE student_id = ?";
+        var inserts = [req.body.student_id];
+        sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+            if(error){
+                console.log("An error occurred while attempting the delete operation");
+                res.redirect('/student');
+            } else {
+                res.redirect('/student');
+            }
+        });
+    })
+
+    router.post('/', function(req, res){
+        if(req.body.name && req.body.blood && req.body.dorm && req.body.locatedAt){
+            var mysql = req.app.get('mysql');
+            var sql = "INSERT INTO student (name, blood, dorm, locatedAt) VALUES (?,?,?,?)";
+            var inserts = [req.body.name, req.body.blood, req.body.dorm, req.body.locatedAt];
+            sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+                if(error){
+                    console.log("An error occurred when attempting to add a new student");
+                    res.end();
+                } else {
+                    res.redirect('/student');
+                }
+            });
+        } else {
+            res.redirect('/student');
+        }
+    })
 
     router.get('/', function(req, res){
         var context = {};
